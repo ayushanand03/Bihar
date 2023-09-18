09546f260f4aa89392a21840b9aee5d87e394b5d
ayushanand03 <145312360+ayushanand03@users.noreply.github.com>
2023-09-18 09:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,23 +17,25 @@ st.title('Comparison of Hindu and Muslim Populations in Bihar')
 num_entries = 50
 data = generate_sample_data(num_entries)
 
-# Display the data
-st.write('Population Data:')
-st.write(data)
-
 # Plot a bar chart for population comparison
-st.write('### Population Comparison')
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12, 6))  # Adjust the figure size
 x = np.arange(num_entries)
-width = 0.35
-ax.bar(x, data['Population'][0], width, label='Hindu')
-ax.bar(x + width, data['Population'][1], width, label='Muslim')
-ax.set_xlabel('Entry')
-ax.set_ylabel('Population')
-ax.set_title('Population Comparison')
-ax.set_xticks(x + width / 2)
-ax.set_xticklabels([f'{i+1}' for i in range(num_entries)])
+width = 0.4  # Adjust the bar width
+
+ax.bar(x - width/2, data['Population'][0], width, label='Hindu')
+ax.bar(x + width/2, data['Population'][1], width, label='Muslim')
+
+ax.set_xlabel('Entry', fontsize=12)
+ax.set_ylabel('Population', fontsize=12)
+ax.set_title('Population Comparison', fontsize=14)
+ax.set_xticks(x)
+ax.set_xticklabels([f'Entry {i+1}' for i in range(num_entries)], rotation=45, ha='right')
+
+# Add legend and grid
 ax.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
+ax.grid(True)
+
+plt.tight_layout()  # Adjust layout
+
+# Display the plot using Streamlit
 st.pyplot(fig)

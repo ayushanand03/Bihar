74902b45d23c903ee7ce70ffe08f44ec14f63fa0
ayushanand03 <145312360+ayushanand03@users.noreply.github.com>
2023-09-18 10:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,29 +17,54 @@ st.title('Comparison of Hindu and Muslim Populations in Bihar')
 num_entries = 50
 data = generate_sample_data(num_entries)
 
-# Allow user to select a location
-selected_location = st.selectbox('Select a location:', [f'Entry {i+1}' for i in range(num_entries)])
+# Allow user to select whether to display all entries
+display_all = st.checkbox('Display All Entries')
 
-# Get the index of the selected location
-location_index = int(selected_location.split()[1]) - 1
+if display_all:
+    # Plot a grouped bar chart for population comparison for all entries
+    fig, ax = plt.subplots(figsize=(12, 6))
+    width = 0.4
 
-# Plot a bar chart for population comparison for the selected location
-fig, ax = plt.subplots(figsize=(12, 6))
-width = 0.4
+    ax.bar(np.arange(2) - width/2, data['Population'][0], width, label='Hindu', color='orange')
+    ax.bar(np.arange(2) + width/2, data['Population'][1], width, label='Muslim', color='#aaffaa')
 
-ax.bar(np.arange(2) - width/2, data['Population'][0][location_index], width, label='Hindu', color='orange')
-ax.bar(np.arange(2) + width/2, data['Population'][1][location_index], width, label='Muslim', color='#aaffaa')
+    ax.set_xlabel('Religion', fontsize=12)
+    ax.set_ylabel('Population', fontsize=12)
+    ax.set_title('Population Comparison for All Entries', fontsize=14)
+    ax.set_xticks(np.arange(2))
+    ax.set_xticklabels(['Hindu', 'Muslim'])
 
-ax.set_xlabel('Religion', fontsize=12)
-ax.set_ylabel('Population', fontsize=12)
-ax.set_title(f'Population Comparison for {selected_location}', fontsize=14)
-ax.set_xticks(np.arange(2))
-ax.set_xticklabels(['Hindu', 'Muslim'])
+    ax.legend()
+    ax.grid(True)
 
-ax.legend()
-ax.grid(True)
+    plt.tight_layout()
 
-plt.tight_layout()
+    # Display the plot using Streamlit
+    st.pyplot(fig)
+else:
+    # Allow user to select a specific location
+    selected_location = st.selectbox('Select a location:', [f'Entry {i+1}' for i in range(num_entries)])
 
-# Display the plot using Streamlit
-st.pyplot(fig)
+    # Get the index of the selected location
+    location_index = int(selected_location.split()[1]) - 1
+
+    # Plot a bar chart for population comparison for the selected location
+    fig, ax = plt.subplots(figsize=(12, 6))
+    width = 0.4
+
+    ax.bar(np.arange(2) - width/2, data['Population'][0][location_index], width, label='Hindu', color='orange')
+    ax.bar(np.arange(2) + width/2, data['Population'][1][location_index], width, label='Muslim', color='#aaffaa')
+
+    ax.set_xlabel('Religion', fontsize=12)
+    ax.set_ylabel('Population', fontsize=12)
+    ax.set_title(f'Population Comparison for {selected_location}', fontsize=14)
+    ax.set_xticks(np.arange(2))
+    ax.set_xticklabels(['Hindu', 'Muslim'])
+
+    ax.legend()
+    ax.grid(True)
+
+    plt.tight_layout()
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
